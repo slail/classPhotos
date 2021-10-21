a681bbcf8826991f5db17a047911b86bf36e928d
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-20 20:16:32 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def classPhotos(redShirtHeights, blueShirtHeights):
+    """
+    You're given two input arrays: one containing the heights of all the students with red shirts
+    and another one containing the heights of all the students with blue shirts.
+    These arrays will always have the same length, and each height will be a positive integer.
+    Write a function that returns whether or not a class photo that follows the stated guidelines can be taken.
+    Note: you can assume that each class has at least 2 students.
+    """
+    pass
+
